4818f9ab5ad11f15be5277e6d2e8a3d76f902329
Dan <decisivedaniel@proton.me>
2023-10-29 20:32:30 +0100

    Add route holding to frame and return routes if it is part of a route

--- src/transport/logic/TrainFrame.scala
+++ src/transport/logic/TrainFrame.scala
@@ -2,7 +2,9 @@ package transport.logic
 
 import transport.logic.resources.KeyPoint
 
-case class TrainFrame(cursor: Point, paths: List[Point], keyPoints: List[KeyPoint], money: Int, score: Int) {
+import scala.collection.immutable.Queue
+
+case class TrainFrame(cursor: Point, paths: List[Point], routes: List[Queue[Point]], keyPoints: List[KeyPoint], money: Int, score: Int) {
   private val costOfTrack: Int = 10
   private val sellPenalty: Int = 1
 
@@ -12,12 +14,14 @@ case class TrainFrame(cursor: Point, paths: List[Point], keyPoints: List[KeyPoin
     copy(gridDim.withinBounds(cursor + d.toPoint))
   }
 
+  def getCursor(): Point = cursor
+
   def getCellType(p: Point): CellType = {
     if (cursor == p) Cursor()
     else if (keyPoints.exists(kp => kp.isLocatedHere(p))) {
       val kp = keyPoints.find(kp => kp.isLocatedHere(p)).head
       DisplayCity(kp.toString)
-    }
+    } else if (routes.exists(route => route.contains(p))) Route()
     else if (paths.contains(p)) Track()
     else Empty()
   }
